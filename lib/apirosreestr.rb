bbcc3d17026f49f4ae6f6feb9fd95708c5e268b3
Dmitry Silaev <dmsilaev@icloud.com>
2017-03-10 23:27:17 +0300

    init commit

---
+++ lib/apirosreestr.rb
@@ -0,0 +1,15 @@
+require "apirosreestr/version"
+
+module Apirosreestr
+  class << self
+    attr_writer :configuration
+  end
+
+  def self.configuration
+    @configuration ||= Configuration.new
+  end
+
+  def self.configure
+    yield(configuration)
+  end
+end
---
+++ lib/apirosreestr/version.rb
@@ -0,0 +1,3 @@
+module Apirosreestr
+  VERSION = "0.1.0"
+end
